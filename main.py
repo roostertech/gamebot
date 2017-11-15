07a19547f1ea53ee449dceb50115b3e64c546d62
Phuong Nguyen <phuong@itsoninc.com>
2017-11-14 17:03:40 -0800

    Refactor recognizer for multiple uses

--- main.py
+++ main.py
@@ -1,54 +1,118 @@
 import numpy as np
 import time
+import random
 from com.dtmilano.android.viewclient import ViewClient
-from recognizer import GridRecognizer
+from recognizer import Recognizer
 from PIL import Image
 
 
-
-def get_cell_center(x, y):
+def get_cell_center(board_box, cell_size, x, y):
     return (board_box[0] + x * cell_size + cell_size / 2, board_box[1] + y * cell_size + cell_size / 2)
 
-board_box = (21,340,1059,1724)
-board_size = (6,8)
-game_board = np.zeros((board_size[0], board_size[1]), dtype=np.int32)
+
+def get_map_cell_center(x, y):
+    return get_cell_center(map_box, map_cell_size, x, y)
+
+
+def get_battle_cell_center(x, y):
+    return get_cell_center(board_box, cell_size, x, y)
+
+
 board_dict = {0: 'Empty ', 1: 'Mob ', 2: 'Player '}
+
+board_box = (21, 340, 1059, 1724)
+board_size = (6, 8)
 cell_size = 173
 
-cell_locations = []
-for column in range(0,board_size[0]):
-    for row in range(0,board_size[1]):
-        cell_locations.append(get_cell_center(column, row))
-
-for index in range(0, 48):
-    print str(index) + ' ' + str(cell_locations[index])
-
+map_size = (9, 12)
+map_box = (0, 180, 1080, 1626)
+map_cell_size = 120
+#
 # device, serialno = ViewClient.connectToDeviceOrExit(verbose=True, serialno='34eda071')
 # print(serialno)
+
+
 # device.takeSnapshot().save('training/map.png', 'PNG')
 
 
-def takeSnapShot():
-    device.takeSnapshot().save('training/snap1.png', 'PNG')
-
-
 def print_board(board):
+    print "-----------"
     for line in board.transpose():
         for elem in line:
             print board_dict[elem] + ' ',
         print
+    print "-----------"
+
+def send_battle_drag(cell_A, cell_B, randomFactor=10):
+    x1 = cell_A[0] + random.randrange(-randomFactor, randomFactor)
+    y1 = cell_A[1] + random.randrange(-randomFactor, randomFactor)
+    x2 = cell_B[0] + random.randrange(-randomFactor, randomFactor)
+    y2 = cell_B[1] + random.randrange(-randomFactor, randomFactor)
+
+    print "Dragging from cells : (%d, %d) to (%d, %d)" % (x1, y1, x2, y2)
+    device.drag((x1, y1), (x2, y2), 100, 10)
 
 
-def send_drag(cell_A,cell_B):
-    print "Dragging from cells : " + str(cell_A) + " to " + str(cell_B)
-    device.drag((cell_A[0], cell_A[1]), (cell_B[0], cell_B[1]), 100, 10)
-    time.sleep(1)
+battle_recognizer = Recognizer("battle")
+battle_recognizer.load('training/battle/player', 2)
+battle_recognizer.load('training/battle/enemy', 1)
+battle_recognizer.load('training/battle/boss', 1)
+battle_recognizer.load('training/battle/empty', 0)
+battle_recognizer.train()
 
-recognizer = GridRecognizer()
-recognizer.train()
+map_recognizer = Recognizer("map")
+map_recognizer.load('training/map/player', 2)
+map_recognizer.load('training/map/boss', 1)
+map_recognizer.load('training/map/empty', 0)
+map_recognizer.train()
 
-    # {'width': 1440, 'density': 3.0, 'orientation': 0, 'height': 2560}
+scene_dict = {0: 'Map', 1: 'Battle', 2: 'Load', 3: 'Loot', 4: 'Score'}
+
+scene_recognizer = Recognizer("scene")
+scene_recognizer.load('training/scenes/map', 0)
+scene_recognizer.load('training/scenes/battle', 1)
+scene_recognizer.load('training/scenes/load', 2)
+scene_recognizer.load('training/scenes/loot', 3)
+scene_recognizer.load('training/scenes/score', 4)
+scene_recognizer.train()
+
+
+# {'width': 1440, 'density': 3.0, 'orientation': 0, 'height': 2560}
 # print(device.getDisplayInfo())
+
+
+def parseBoard(rawImage, board_box, board_size, cell_size, recognizer):
+    board_image = rawImage.crop(board_box)
+    board_image.save('out.png')
+
+    return_board = np.zeros((board_size[0], board_size[1]), dtype=np.int32)
+
+    for x in range(0, board_size[0]):
+        for y in range(0, board_size[1]):
+            cell_box = (x * cell_size, y * cell_size, (x + 1) * cell_size, (y + 1) * cell_size)
+            cell_image = board_image.crop(cell_box)
+            return_board[x, y] = recognizer.predict(cell_image)
+            # cell_image.save('cell_%d_%d.png' % (x, y))
+
+    return return_board
+
+
+def parseBattleBoard(rawImage):
+    return parseBoard(rawImage, board_box, board_size, cell_size, battle_recognizer)
+
+def parseMapBoard(rawImage):
+    return parseBoard(rawImage, map_box, map_size, map_cell_size, map_recognizer)
+
+
+# print_board(parseBattleBoard(Image.open('training/scenes/battle/snap1.png')))
+print_board(parseMapBoard(Image.open('training/scenes/map/map.png')))
+
+# send_drag(get_cell_center(1,1), get_cell_center(2,2))
+# send_drag(get_cell_center(1,0), get_cell_center(2,0))
+# for x in range(0,10):
+#     send_battle_drag(get_cell_center(1,1), get_cell_center(3,3))
+
+# device.
 
 # device.drag()
 
@@ -61,20 +125,8 @@ recognizer.train()
 # 21 - 173 * 6 - 21
 #  340 1,384 - 184
 # img = Image.open('training/start.png')
-img = Image.open('training/start.png')
+# img = Image.open('training/scenes/map/map3.png')
+# parseMap(img)
 
-board_image = img.crop(board_box)
-board_image.save('out.png')
-
-for x in range(0, board_size[0]):
-    for y in range(0, board_size[1]):
-        cell_box = (x * cell_size, y * cell_size, (x + 1) * cell_size, (y + 1) * cell_size)
-        cell_image = board_image.crop(cell_box)
-        game_board[x, y] = recognizer.predict(cell_image)
-        # cell_image.save('cell_' + str(x) + '_' + str(y) + '.png')
-        # print 'cell_' + str(x) + '_' + str(y) + ' : ' + str()
-
-print_board(game_board)
-
-# Find player to move
-# if (game_board[2,4] == 2) :
+# cell_image.save('cell_' + str(x) + '_' + str(y) + '.png')
+# print 'cell_' + str(x) + '_' + str(y) + ' : ' + str()
--- recognizer.py
+++ recognizer.py
@@ -4,44 +4,47 @@ from sklearn.externals import joblib
 import os
 import numpy as np
 from PIL import Image
+
+
 # import Pillow
 
-class GridRecognizer:
-    def __init__(self):
+class Recognizer:
+    def __init__(self, name):
         self.training_data = []
         self.target_values = []
         self.svc = svm.SVC(gamma=0.001, kernel='linear', C=100)
         self.downscale_res = (32, 32)
+        self.name = name
 
-    def _load(self, path, target_value):
+    def load(self, path, target_value, verbose=False):
         training_imgs = os.listdir(path)
         for f in training_imgs:
-            img = Image.open(path+'/'+f)
+            if verbose:
+                print "%s -> %d" % (f, target_value)
+            img = Image.open(path + '/' + f)
             img = img.resize(self.downscale_res, Image.BILINEAR)
             self.training_data.append(np.array(img.getdata()).flatten())
             self.target_values.append(target_value)
 
-    def load(self):
-        self._load('training/player', 2)
-        self._load('training/enemy', 1)
-        self._load('training/boss', 1)
-        self._load('training/empty', 0)
+    # def load(self):
+    #     self._load('training/player', 2)
+    #     self._load('training/enemy', 1)
+    #     self._load('training/boss', 1)
+    #     self._load('training/empty', 0)
 
     def train(self):
-        if os.path.isfile('svc.dat'):
-            self.svc = joblib.load('svc.dat')
+        if os.path.isfile('svc-%s.dat' % self.name):
+            self.svc = joblib.load('svc-%s.dat' % self.name)
         else:
-            self.load()
+            # self.load()
             np_data = np.array(self.training_data)
             print (np.array(self.training_data))
             np_values = np.array(self.target_values)
             self.svc.fit(np_data, np_values)
-            joblib.dump(self.svc, 'svc.dat', compress=9)
+            joblib.dump(self.svc, 'svc-%s.dat' % self.name, compress=9)
 
     def predict(self, image):
         resized_img = image.resize(self.downscale_res, Image.BILINEAR)
         np_img = (np.array(resized_img.getdata()).flatten())
-        np_img = np.array(np_img).reshape(1,-1)
+        np_img = np.array(np_img).reshape(1, -1)
         return int(self.svc.predict(np_img))
-
-
